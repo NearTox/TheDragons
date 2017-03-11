007321fbbbdf7dbffd24925aece7fa93954d4595
Alejandro <alex@androidicts.com>
2017-03-11 14:48:45 -0600

    added event cocina

--- control/app/src/main/java/com/thedragons/control/MainActivity.java
+++ control/app/src/main/java/com/thedragons/control/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.ToggleButton;
 
 import com.bezirk.middleware.Bezirk;
 import com.bezirk.middleware.android.BezirkMiddleware;
+import com.thedragons.control.Events.LamparaEvent;
 import com.thedragons.control.models.Lampara;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,11 +16,16 @@ public class MainActivity extends AppCompatActivity {
   private static final int COLOR_AMARILLO = 37;
   private static final int COLOR_AZUL = 38;
   private static final int COLOR_VERDE = 39;
+
   private static final int ENCENDIDO = 1;
   private static final int APAGADO = 0;
   private static final int APAGAR_TODAS = 100;
 
-  private ToggleButton toggleButton;
+  private static final int COCINA = 21;
+  private static final int SALA = 22;
+  private static final int DORMITORIO = 23;
+
+  private ToggleButton toggleCocina;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -28,19 +34,19 @@ public class MainActivity extends AppCompatActivity {
 
     //Initialize the Bezirk service
     BezirkMiddleware.initialize(this);
-    toggleButton = findViewById(R.id.asd);
+    toggleCocina = (ToggleButton) findViewById(R.id.toggleCocina);
 
     //Register with BezirkMiddleware to get an instance of Bezirk API.
     //The parameter is any human-readable string for a name of your Zirk
     bezirk = BezirkMiddleware.registerZirk("Control de lamparas");
 
-    toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+    toggleCocina.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
       @Override
       public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         if (isChecked) {
-          bezirk.sendEvent(new LamparaEvent());
+          bezirk.sendEvent(new LamparaEvent(COCINA, ENCENDIDO, COLOR_AMARILLO));
         } else {
-
+          bezirk.sendEvent(new LamparaEvent(COCINA, APAGADO, COLOR_AMARILLO));
         }
       }
     });
