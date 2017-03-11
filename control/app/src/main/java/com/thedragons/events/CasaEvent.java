f38bc0227b617d264497a8cfd509a0285f164514
Alejandro <alex@androidicts.com>
2017-03-11 16:00:11 -0600

    added eventes

---
+++ control/app/src/main/java/com/thedragons/events/CasaEvent.java
@@ -0,0 +1,8 @@
+package com.thedragons.events;
+
+import com.bezirk.middleware.messages.Event;
+
+public class CasaEvent extends Event {
+
+
+}
---
+++ control/app/src/main/java/com/thedragons/events/LamparaEvent.java
@@ -0,0 +1,20 @@
+package com.thedragons.events;
+
+import com.bezirk.middleware.messages.Event;
+
+public class LamparaEvent extends Event  {
+  private final int id;
+  private final int encendido;
+  private final int color;
+
+  public LamparaEvent(int id, int encendido, int color) {
+    this.id = id;
+    this.encendido=encendido;
+    this.color=color;
+  }
+
+  public int getId() { return id; }
+  public int getEncendido() { return encendido; }
+  public int getColor() { return color; }
+
+}
