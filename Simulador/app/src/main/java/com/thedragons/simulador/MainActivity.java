b6a156554c5e2354165c42e8bbff34cc6f771210
Alejandro <alex@androidicts.com>
2017-03-11 15:12:19 -0600

    added receivers

--- Simulador/app/src/main/java/com/thedragons/simulador/MainActivity.java
+++ Simulador/app/src/main/java/com/thedragons/simulador/MainActivity.java
@@ -2,6 +2,14 @@ package com.thedragons.simulador;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.bezirk.middleware.Bezirk;
+import com.bezirk.middleware.addressing.ZirkEndPoint;
+import com.bezirk.middleware.android.BezirkMiddleware;
+import com.bezirk.middleware.messages.Event;
+import com.bezirk.middleware.messages.EventSet;
+import com.thedragons.simulador.events.LamparaEvent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,21 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    BezirkMiddleware.initialize(this);
+    Bezirk bezirk = BezirkMiddleware.registerZirk("Simulador");
+
+    final EventSet eventSet = new EventSet(LamparaEvent.class);
+
+    eventSet.setEventReceiver(new EventSet.EventReceiver() {
+      @Override
+      public void receiveEvent(Event event, ZirkEndPoint sender) {
+        //Check if the event is of interest
+        if (event instanceof LamparaEvent) {
+          final LamparaEvent lamparaEvent = (LamparaEvent) event;
+          Log.d("*****", String .valueOf(lamparaEvent.getEncendido()));
+          //TODO encender lamparas
+        }
+      }
+    });
   }
 }
