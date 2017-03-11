20e3125474994cc93e2e97cd4912953963a1e1ed
NT - Ernesto Ramirez <NearTox@outlook.com>
2017-03-11 17:45:21 -0600

    ...

--- control/app/src/main/java/com/thedragons/control/models/Lampara.java
+++ control/app/src/main/java/com/thedragons/control/models/Lampara.java
@@ -1,11 +1,15 @@
 package com.thedragons.control.models;
+import com.thedragons.events.LamparaEvent;
 
 public class Lampara {
 
   public int id;
 
-  public Boolean encendido;
+  public int encendido;
 
-  public String color;
+  public int color;
 
+  public LamparaEvent makeEvent(){
+    return new LamparaEvent(id,encendido,color);
+  }
 }
