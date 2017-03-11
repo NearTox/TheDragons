cf4b4b19c86ff4ece53c7f0a09cd61156ba40244
Efrain Rodriguez <efrain@MacBook-Air-de-Efrain.local>
2017-03-11 15:16:22 -0600

    Simulador

--- Simulador/app/src/main/java/com/thedragons/simulador/MainActivity.java
+++ Simulador/app/src/main/java/com/thedragons/simulador/MainActivity.java
@@ -3,10 +3,24 @@ package com.thedragons.simulador;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.bezirk.middleware.Bezirk;
+import com.bezirk.middleware.android.BezirkMiddleware;
+
 public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+
+    Bezirk bezirk;
+
+    //Initialize the Bezirk service
+    BezirkMiddleware.initialize(this);
+
+
+    //Register with BezirkMiddleware to get an instance of Bezirk API.
+    //The parameter is any human-readable string for a name of your Zirk
+    bezirk = BezirkMiddleware.registerZirk("Simulador de lamparas");
+
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
   }
